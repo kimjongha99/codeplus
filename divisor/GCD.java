05f5acecb25e3533f4169336fa59e3633d08c1b7
kimjongha99 <jjhsun99@gmail.com>
2023-05-11 00:44:25 +0900

    최대공약수
    최대공약수

---
+++ divisor/GCD.java
@@ -0,0 +1,24 @@
+package divisor;
+
+//최대공약수
+public class GCD {
+
+   static  int gcd(int a, int b){
+       if(b==0){
+           return a;
+       }else{
+           return gcd(b,a%b);
+       }
+   }
+   static  int lcm(int a, int b){
+       int gcd = gcd(a,b);
+
+       int l= gcd*(a/gcd)*(b/gcd);
+       return l;
+   }
+    public static void main(String[] args) {
+
+        System.out.println(gcd(gcd(10,15),20));
+        System.out.println(lcm(10,15));
+    }
+}
