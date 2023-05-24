fe8ee5ff91b92b20bde2d120a88732c8f025f39d
kimjongha99 <jjhsun99@gmail.com>
2023-05-24 17:04:10 +0900

    피자 나눠 먹기 (1)

---
+++ Programmers/P120814.java
@@ -0,0 +1,20 @@
+package Programmers;
+
+
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/120814
+피자 나눠 먹기 (1)
+
+ */
+public class P120814 {
+
+
+    static int solution(int n) {
+
+
+    }
+    public static void main(String[] args) {
+
+
+    }
+}
