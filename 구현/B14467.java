3e0d7df34f3494b58abcbed26970f7f003cfaf15
kimjongha99 <jjhsun99@gmail.com>
2023-07-12 16:17:04 +0900

    소가 길을 건너간 이유1

---
+++ 구현/B14467.java
@@ -0,0 +1,26 @@
+package 구현;
+
+import java.util.Scanner;
+
+/*
+https://www.acmicpc.net/problem/14467
+소가 길을 건너간 이유1
+ */
+public class B14467 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int [][] arr=  new int[11][1];
+
+        int cow_sum =0;
+
+        for (int i = 1; i < 11; i++) {
+
+        }
+    }
+
+
+}
