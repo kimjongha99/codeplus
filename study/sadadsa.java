2f976a4c787ec0c4c2be4126acf368e8bd8f6811
kimjongha99 <jjhsun99@gmail.com>
2023-07-18 15:39:42 +0900

    ,

---
+++ study/sadadsa.java
@@ -0,0 +1,8 @@
+package study;
+
+public class sadadsa {
+    public static void main(String[] args) {
+        int arr[][] =new int[][]{{1,2,5},{3,5}};
+        System.out.println(arr[0].length);
+    }
+}
