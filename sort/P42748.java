c7996f787258e21571f2099aaa69aabe29e765e3
kimjongha99 <jjhsun99@gmail.com>
2023-08-30 17:02:31 +0900

    Create P42748.java

---
+++ sort/P42748.java
@@ -0,0 +1,20 @@
+package sort;
+
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/42748
+
+K번째수
+ */
+public class P42748 {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = {};
+        return answer;
+    }
+    public static void main(String[] args) {
+        int array[] = new int[]{1, 5, 2, 6, 3, 7, 4};
+        int commands[][] = new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+
+        P42748 sol = new P42748();
+        System.out.println(sol.solution(array,commands));
+    }
+}
