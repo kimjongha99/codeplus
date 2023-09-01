b3ec0d3ad1cc248f423c78ccbc162d4937cb4173
kimjongha99 <jjhsun99@gmail.com>
2023-09-01 15:04:46 +0900

    B11724

--- DFS/B11724.java
+++ DFS/B11724.java
@@ -1,8 +1,6 @@
 package DFS;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
@@ -13,47 +11,55 @@ https://www.acmicpc.net/problem/11724
 
  */
 public class B11724 {
-    static ArrayList<Integer>[] a;
-    static boolean visited[];
+    static  boolean graph[][];
+    static  boolean visited[];
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+   static int N,M;
+    static  int answer;
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        a = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
-
-        for (int i = 1; i < n + 1; i++) { // 인접리스트 초기화
-            a[i] = new ArrayList<Integer>();
-        }
-        for (int i = 1; i < m; i++) {
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            a[u].add(v);// 양 방향 간선이므로 양쪽으로 간선을 더 해준다
-            a[v].add(u);
-        }
-        int count = 0;
-        for (int i = 1; i < n + 1; i++) {
-            if (!visited[i]) {
-                count++;
-                DFS(i);
+    static void dfs(int idx) {
+        visited[idx] = true;
+        for (int i = 1; i <= N; i++) {
+            if (visited[i] == false && graph[idx][i]) {
+                dfs(i);
             }
         }
-        System.out.println(count);
-    }
 
-    private static void DFS(int v) {
-        if (visited[v]) {
-            return;
+    }
+        public static void main(String[] args) throws IOException {
+        //1.입력초기화
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M= Integer.parseInt(st.nextToken());
+
+        //1. 그래프 연결정보주기
+        //그래프의 수
+        graph =new boolean[N+1][N+1];
+        //방문수
+        visited = new boolean[N+1];
+        //1. 그래프 정보 입력.
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            graph[x][y] = graph[y][x] = true;
         }
-        visited[v] = true;
-        for (int i : a[v]) {
-            if (visited[i] == false) { // 연결 정점 중 방문하지 않았던 정점만 탐색함
-                DFS(i);
+
+        //2. dfs 코드작성
+        for (int i = 1; i <= N; i++) {
+            if(!visited[i]) {
+                dfs(i);
+                answer++;
             }
         }
+
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
     }
-}
+
+
+    }
