864318793792ee5e7ee9b0e6a18cd251d919a631
kimjongha99 <jjhsun99@gmail.com>
2023-09-29 22:15:12 +0900

    침투 다시풀어보기

--- DFS/B13565.java
+++ DFS/B13565.java
@@ -11,72 +11,67 @@ https://www.acmicpc.net/problem/13565
 침투
  */
 public class B13565 {
-    static  int MAX =1000+10;
+    static final int MAX = 1000 + 10;
+    static boolean[][] map;
+    static boolean[][] visited;
+    static int N, M;
+    static boolean answer;  // 왜 boolean 배열로 쓰나요?
 
-    static  boolean map [][] ;
-    static  boolean visited[][];
-
-    static  int M,N;  // m-가로 n-세로
-    static int dirY[] = {-1, 1, 0, 0};
-    static  int dirX[]  = {0,0,-1,1};
-
-    static int T,K;
-    static  boolean answer;
-
-    public static  void  dfs(int Y,int X){
-        if(Y==N){
-            answer =true;
-            return;
-
-        }
-        visited[Y][X] = true;
-        for (int i = 0; i < 4; i++) {
-            int newY = Y + dirY[i];
-            int newX = X + dirX[i];
-
-            if(map[newY][newX]&&visited[newY][newX]==false){
-                dfs(newY,newX);
-            }
-
-        }
-
-    }
+    static int[] dirY = {-1, 1, 0, 0};
+    static int[] dirX = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N =Integer.parseInt(st.nextToken());
-        M=  Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
 
         map = new boolean[MAX][MAX];
         visited = new boolean[MAX][MAX];
 
-
-        ///맵 정보 반영
+        //맵정보 저장
         for (int i = 1; i <= N; i++) {
             String str = br.readLine();
-            for (int j = 1; j <= M; j++)
-                map[i][j]= (str.charAt(j-1)=='0'?true:false);
+            for (int j = 1; j <= M; j++) {
+                map[i][j] = (str.charAt(j - 1) == '0' ? true : false);
+            }
         }
 
-            //dfs
-            for (int j = 1; j <= M; j++) {
-                if(map[1][j]){
-                    dfs(1,j);
-                }
+        //dfs
+        for (int j = 1; j <= M; j++) {
+            if (map[1][j]) {          // 왜 map[1][j] 일까요
+                dfs(1, j);           //  또한 dfs도왜 1,j 일가요
             }
+        }
 
-
-
-
-
-
-        if (answer) bw.write("YES");
-        else  bw.write("NO");
-
-        bw.flush();
-        bw.close();
+        if (answer){
+            System.out.print("YES");
+        }else
+            System.out.print("NO");
     }
+
+    static void dfs(int y, int x) {
+        if (y == N) {
+            answer = true;
+            return;
+        }
+
+        visited[y][x] = true;
+        for (int i = 0; i < 4; i++) {
+            int newY = y + dirY[i];
+            int newX = x + dirX[i];
+
+            if (map[newY][newX] && visited[newY][newX] == false) {
+                dfs(newY, newX);
+            }
+        }
+
+
+
+    }
+
 }
+
