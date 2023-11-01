b527553c413a2069133492948addee3481a4b4cd
kimjongha99 <jjhsun99@gmail.com>
2023-11-01 17:48:03 +0900

    격자판 칠하기

---
+++ SwExpertAcademy/S1288.java
@@ -0,0 +1,13 @@
+package SwExpertAcademy;
+
+
+/*
+https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2&contestProbId=AV18_yw6I9MCFAZN&categoryId=AV18_yw6I9MCFAZN&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=JAVA&select-1=2&pageSize=10&pageIndex=3
+
+새로운 불면증 치료법
+ */
+public class S1288 {
+    public static void main(String[] args) {
+
+    }
+}
--- SwExpertAcademy/S14413.java
+++ SwExpertAcademy/S14413.java
@@ -2,9 +2,7 @@
 package SwExpertAcademy;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 /*
@@ -14,64 +12,80 @@ https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=3&co
  */
 public class S14413 {
     static  char [][] map;
-    // 방향 벡터를 전역으로 선언
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
+    static  int [] dirY ={-1,1,0,0};
+    static  int [] dirX ={0,0,1,-1};
 
-    // 주어진 좌표와 색상에 대해 인접한 셀들이 동일한 색상을 갖는지 검사
-    static boolean checkAdjacent(int x, int y, char color, int N, int M) {
-        for (int dir = 0; dir < 4; dir++) {
-            int nx = x + dx[dir], ny = y + dy[dir];
-            if (nx >= 0 && nx < N && ny >= 0 && ny < M && map[nx][ny] == color) {
-                return false;
+    static void dfs(int y, int x, int N, int M) {
+        for (int i = 0; i < 4; i++) {
+            int newY = y+dirY[i];
+            int newX = x+dirX[i];
+
+            if (newY < 0 || newX < 0 || newY >= N || newX >= M) {
+                continue;
+            }
+            if (map[newY][newX] == '.') {
+                map[y][x] = '#';
+            }
+            if (map[newY][newX] == '#') {
+                map[y][x] = '.';
             }
         }
-        return true;
-
     }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st =new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int tc = Integer.parseInt(st.nextToken());
 
-        int t = Integer.parseInt(st.nextToken());
+        for (int t = 1; t <= tc; t++) {
+            st = new StringTokenizer(br.readLine());
 
-        for (int tc = 1; tc <= t; tc++) {
-            st= new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
+
             map = new char[N][M];
+
             for (int i = 0; i < N; i++) {
-                String  str=  br.readLine();
+                String str = br.readLine();
                 for (int j = 0; j < M; j++) {
                     map[i][j]= str.charAt(j);
                 }
             }
 
-            boolean possible = true;
-            for (int i = 0; i < N && possible; i++) { // 'possible'을 조건으로 추가
+            for (int i = 0; i < N; i++) {
                 for (int j = 0; j < M; j++) {
                     if (map[i][j] == '?') {
-                        if (checkAdjacent(i, j, '#', N, M)) {
-                            map[i][j] = '#';
-                        } else if (checkAdjacent(i, j, '.', N, M)) {
-                            map[i][j] = '.';
-                        } else {
-                            possible = false;
-                            break;
-                        }
+                        dfs(i,j,N,M);
                     }
                 }
             }
 
-            System.out.println("#" + tc + " " + (possible ? "possible" : "impossible"));
+            //이제 검증
+            String ans ="possible";
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M - 1; j++) {
+                        if (map[i][j] == '?') continue;
+                    if (map[i][j] == map[i][j + 1]) {
+                        ans = "impossible";
+                    }
+                }
+            }
+            for (int j = 0; j < M; j++) {
+                for (int i = 0; i < N - 1; i++) {
+                    if (map[i][j] == '?') continue;
+                    if (map[i][j] == map[i + 1][j]) {
+                        ans = "impossible";
+                    }
+                }
+            }
 
+            bw.write("#"+t+" "+ans);
+            bw.newLine();
 
 
         }
-
-
-    }
-
+        br.close();
+        bw.close();
+        }
 }
