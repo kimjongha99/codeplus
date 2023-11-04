2fbc6c274756db660111a071fff135a4e1e77f5c
kimjongha99 <jjhsun99@gmail.com>
2023-11-05 00:42:26 +0900

    1974. 스도쿠 검증

--- SwExpertAcademy/S1974.java
+++ SwExpertAcademy/S1974.java
@@ -12,38 +12,87 @@ https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2&co
 
  */
 public class S1974 {
-    static  int [][] arr;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());
+    static int[][] arr;
 
-        int tc = Integer.parseInt(st.nextToken());
-
-        for (int T = 1; T <= tc; T++) {
-
-            arr = new int[9][9];
-
-            for (int i = 0; i < 9; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < 9; j++) {
-                    int num = Integer.parseInt(st.nextToken());
-                    arr[i][j]= num;
-                }
-
+    static int findZull(int[][] arr) {
+        int sumZul = 0;
+        for (int y = 0; y < arr.length; y++) {
+            for (int x = 0; x < arr[y].length; x++) {
+                sumZul += arr[y][x];
             }
+        }
 
-            for(int i=0; i<arr.length; i++) {
-                for(int j=0; j<arr.length; j++) {
-                    System.out.print(arr[i][j] + " ");
-                }
-                System.out.println(); // 한 행이 끝나면 줄바꿈
+        if (sumZul == 405) {
+            return 1;
+        } else
+            return 0;
+    }
+
+    static int findHang(int[][] arr) {
+        int sumHang = 0;
+        for (int x = 0; x < arr.length; x++) {
+            for (int y = 0; y < arr[x].length; y++) {
+                sumHang += arr[y][x];
             }
+        }
+        if (sumHang == 405) {
+            return 1;
+        } else
+            return 0;
+    }
 
+    static boolean findThree(int[][] arr) {
+        for (int y = 0; y < 9; y += 3) {
+            for (int x = 0; x < 9; x += 3) {
+                boolean[] blockCheck = new boolean[9];
+                for (int i = y; i < y + 3; i++) {
+                    for (int j = x; j < x + 3; j++) {
+                        if (arr[i][j] < 1 || arr[i][j] > 9 || blockCheck[arr[i][j] - 1]) {
+                            return false;
+                        }
+                        blockCheck[arr[i][j] - 1] = true;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+            public static void main (String[]args) throws IOException {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                StringTokenizer st = new StringTokenizer(br.readLine());
+
+                int tc = Integer.parseInt(st.nextToken());
+
+                for (int T = 1; T <= tc; T++) {
+
+                    arr = new int[9][9];
+
+                    for (int i = 0; i < 9; i++) {
+                        st = new StringTokenizer(br.readLine());
+                        for (int j = 0; j < 9; j++) {
+                            int num = Integer.parseInt(st.nextToken());
+                            arr[i][j] = num;
+                        }
+
+                    }
+
+                    int hanZull = findZull(arr);
+
+                    int hanhHang = findHang(arr);
+
+                    boolean threeThree = findThree(arr);
+
+                    if (hanZull == hanhHang == threeThree) {
+                        System.out.println("1");
+                    }else System.out.println("0");
+
+                }
+            }
         }
 
 
-    }
-}
 /*
 2차원배열값보기
    for(int i=0; i<arr.length; i++) {
