ede1a3b552e5e3cf1197d2b36e7249329da57f1f
kimjongha99 <jjhsun99@gmail.com>
2023-11-08 14:43:33 +0900

    Update Test.java

--- SwExpertAcademy/Test.java
+++ SwExpertAcademy/Test.java
@@ -1,23 +1,49 @@
 package SwExpertAcademy;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        int[][] arr = {
-                {13, 24, 13, 24, 1},
-                {26, 14, 19, 3, 26},
-                {11, 9, 29, 13, 26},
-                {20, 28, 29, 21, 27},
-                {20, 3, 28, 28, 1}
-        };
+            Scanner sc = new Scanner(System.in);
+            int T = sc.nextInt(); // 테스트 케이스의 수
 
-        int rows = arr.length;
-        int cols = arr[0].length;
+            for (int testCase = 1; testCase <= T; testCase++) {
+                int N = sc.nextInt(); // 손님의 수
+                int M = sc.nextInt(); // 붕어빵을 만드는 데 걸리는 시간
+                int K = sc.nextInt(); // M초에 만들어지는 붕어빵의 수
 
-        for (int i = 0; i < rows; i++) { // 각 행을 순회
-            for (int j = 0; j < cols; j++) { // 각 열을 순회
-                System.out.print("행 " + i + ", 열 " + j + " 의 값: " + arr[i][j] + "  |  ");
+                int[] arrivalTimes = new int[N];
+                for (int i = 0; i < N; i++) {
+                    arrivalTimes[i] = sc.nextInt();
+                }
+
+                // 손님들의 도착 시간을 정렬
+                Arrays.sort(arrivalTimes);
+
+                boolean possible = true;
+                int bungeoppangMade = 0; // 현재까지 만들어진 붕어빵의 수
+                int timeForNextBungeoppang = 0; // 다음 붕어빵이 만들어지는 시간
+
+                for (int arrivalTime : arrivalTimes) {
+                    // 다음 붕어빵이 만들어질 때까지 기다림
+                    while (timeForNextBungeoppang <= arrivalTime) {
+                        bungeoppangMade += K;
+                        timeForNextBungeoppang += M;
+                    }
+
+                    // 손님이 도착했을 때 붕어빵이 준비되어 있지 않으면
+                    if (--bungeoppangMade < 0) {
+                        possible = false;
+                        break;
+                    }
+                }
+
+                System.out.println("#" + testCase + " " + (possible ? "Possible" : "Impossible"));
             }
-            System.out.println(); // 각 행의 끝에서 줄바꿈
-        }
+
+
+        sc.close();
     }
-}
+    }
+
