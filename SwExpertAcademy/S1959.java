3e061730612cda2aeeb442c766311ce6811f1515
kimjongha99 <jjhsun99@gmail.com>
2023-11-08 17:46:36 +0900

    두개의 숫자열 re

--- SwExpertAcademy/S1959.java
+++ SwExpertAcademy/S1959.java
@@ -1,9 +1,7 @@
 package SwExpertAcademy;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -13,48 +11,52 @@ https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2&co
 
  */
 public class S1959 {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-
-        int arrA[] = new int[A];
-        int arrB[] = new int[B];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < arrA.length; i++) {
-            arrA[i] = Integer.parseInt(st.nextToken());
-        }
-        st=new StringTokenizer(br.readLine());
-        for (int i = 0; i < arrB.length; i++) {
-            arrB[i] = Integer.parseInt(st.nextToken());
-        }
-
-        // 최대 합을 계산합니다.
-        int maxSum = findMaxProductSum(arrA, arrB);
-        System.out.println(maxSum);
-    }
-
-    private static int findMaxProductSum(int[] A, int[] B) {
-        // 더 짧은 배열과 더 긴 배열을 구분합니다.
-        int[] shorter = A.length < B.length ? A : B;
-        int[] longer = A.length < B.length ? B : A;
-
+    static int findMax(int[] arrA, int[] arrB) {
         int maxSum = Integer.MIN_VALUE;
 
+        int shorter[] = arrA.length> arrB.length ? arrB:arrA;
+        int Longer[] = arrA.length>arrB.length? arrA:arrB;
+
         // 더 긴 배열에 대해 반복합니다.
-        for (int start = 0; start <= longer.length - shorter.length; start++) {
-            int currentSum = 0;
-            // 더 짧은 배열의 각 요소를 더 긴 배열의 해당 위치와 곱합니다.
-            for (int i = 0; i < shorter.length; i++) {
-                currentSum += shorter[i] * longer[start + i];
+        for (int start = 0; start <= Longer.length - shorter.length; start++) {
+            int currentSum =0;
+            for (int i = 0; i< shorter.length; i++) {
+                currentSum += shorter[i]*Longer[start+i];
             }
-            // 최대 합을 갱신합니다.
-            maxSum = Math.max(maxSum, currentSum);
+            maxSum = Math.max(maxSum,currentSum);
         }
 
+
         return maxSum;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int tc = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= tc; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            int arrA[] = new int[A];
+            int arrB[] = new int[B];
+            st= new StringTokenizer(br.readLine());
+            for (int i = 0; i < A; i++) {
+                arrA[i] = Integer.parseInt(st.nextToken());
+            }
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < B; i++) {
+                arrB[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int maxSum = findMax(arrA,arrB);
+
+            bw.write("#"+t+" "+maxSum);
+            bw.newLine();
+
+
+        }
+        bw.close();
+        br.close();
     }
 }
