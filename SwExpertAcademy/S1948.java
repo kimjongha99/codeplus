0f1466c95eeb7f39da3daa47c3df98bf4716f68a
kimjongha99 <jjhsun99@gmail.com>
2023-11-08 23:29:29 +0900

    날짜 계산기

---
+++ SwExpertAcademy/S1948.java
@@ -0,0 +1,37 @@
+package SwExpertAcademy;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2&contestProbId=AV5PnnU6AOsDFAUq&categoryId=AV5PnnU6AOsDFAUq&categoryType=CODE&problemTitle=&orderBy=RECOMMEND_COUNT&selectCodeLang=JAVA&select-1=2&pageSize=10&pageIndex=2
+
+날짜 계산기
+ */
+public class S1948 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= tc; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int month = Integer.parseInt(st.nextToken());
+            int day = Integer.parseInt(st.nextToken());
+            int nextMonth = Integer.parseInt(st.nextToken());
+            int nextDay = Integer.parseInt(st.nextToken());
+
+
+            int duration
+
+
+
+
+
+
+
+        }
+    }
+}
