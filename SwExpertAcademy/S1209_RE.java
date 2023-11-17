50ea889dc0e367a70ef6d6d86dd8d7f9afab3647
kimjongha99 <jjhsun99@gmail.com>
2023-11-17 12:57:16 +0900

    Create S1209_RE.java

---
+++ SwExpertAcademy/S1209_RE.java
@@ -0,0 +1,35 @@
+package SwExpertAcademy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S1209_RE {
+    static  int [][] map;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= tc; t++) {
+
+            map = new int[3][3];
+
+            for (int i = 0; i < 3; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < 3; j++) {
+                    map[i][j]=Integer.parseInt(st.nextToken());
+                }
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
